647f482aee8d8236ea859cd4938d915b3dfa0001
FatihAksoy530 <fatihaksoy1999@hotmail.com>
2023-09-07 01:49:12 +0300

    font-family added

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './css_reset.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import './css_reset.css';
+import './variables.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
