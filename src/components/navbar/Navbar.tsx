6e7e4f051a036fb682c947251ffc8527538b8903
FatihAksoy530 <fatihaksoy1999@hotmail.com>
2023-09-09 16:35:02 +0300

    Sidebar added.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import "./Navbar.css";
 
 export default function Navbar() {
+    const [navbarOpenClass, setNavbarOpenClass] = useState<string>("");
+    const [overlayClass, setOverlayClass] = useState<string>("");
+
+    const handleNavbarOpenClassChange = (open: boolean) => {
+        if (open) {
+            setNavbarOpenClass("navbar-open");
+            setOverlayClass("overlay-open");
+        } else {
+            setNavbarOpenClass("");
+            setOverlayClass("");
+        }
+    }
+    
+
     return (
-        <nav className="navbar-container">
-            <Link className='to-home-btn' to="/">Home</Link>
-            <h1>Navbar</h1>
-        </nav>
+        <>
+            <div className= {`overlay ${overlayClass}`} id='overlay' onClick={()=>{handleNavbarOpenClassChange(false)}}></div>
+            <div className={`navbar-container ${navbarOpenClass}`}>
+                <nav className="navbar">
+                    <Link className='navbar-btn' to="/">Home</Link>
+                </nav>
+            </div>
+            <div className='sidebar-icon-container' onClick={()=>{handleNavbarOpenClassChange(true)}}>
+                <button className='sidebar-icon'><ArrowForwardIosIcon/></button>
+            </div>
+        </>
     )
 }
