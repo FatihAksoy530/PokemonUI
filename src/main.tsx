22521f54727f508d0350bec1a1dd23092abc7a23
FatihAksoy530 <fatihaksoy1999@hotmail.com>
2023-09-12 23:32:13 +0300

    Remove unnecessary lines.

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,7 @@ import {
 } from "react-router-dom";
 import Root from './routes/root/Root.tsx';
 import ErrorPage from './ErrorPage.tsx';
-import CardDetailsPage, {
-  loader as cardDetailsPageLoader,
-} from './components/card_details_page/CardDetailsPage.tsx';
+import CardDetailsPage from './components/card_details_page/CardDetailsPage.tsx';
 import HomePage from './routes/home/HomePage.tsx';
 import './index.css';
 import './css_reset.css';
@@ -27,7 +25,6 @@ const router  = createBrowserRouter([
       {
         path: "cards/:cardId",
         element: <CardDetailsPage />,
-        loader: cardDetailsPageLoader,
       },
     ]
   }
